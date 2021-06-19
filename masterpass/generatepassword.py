ca53a922e846a0d8e1ee6442d90983cc32443a20
yunus-emre99 <yunus-emre57@hotmail.de>
2021-06-20 00:53:46 +0200

    verbesserung

--- masterpass/generatepassword.py
+++ masterpass/generatepassword.py
@@ -1,50 +1,33 @@
 import random
 import string
-import pyperclip
-import getpass
-
-random.random()
-###################################
-
-inhalt = ''
-tester1=0;
-tester2=0;
-tester3=0;
-tester4=0;
-
-#ABFRAGE/INHALT PASSWORT
-laenge = int(input("Laenge: "))
-klein = input("Kleinbuchstabe (j/n): ")
-gross = input("Grossbuchstaben (j/n): ")
-zahl = input("Zahlen (j/n): ")
-zeichen = input("Sonderzeichen (j/n): ")
+import inquirer
 
 
+def _generate_password():
+    laenge = int(input("Laenge: "))
 
-#SICHERHEITSLEVEL DES PASSWORTS
+    abfrage = [
+        inquirer.Checkbox('Passwort',
+                          message="Was soll ihr generiertes Passwort beeinhalten",
+                          choices=['Kleinbuchstaben', 'Grossbuchstaben', 'Zahlen[0-9]', 'Zeichen'],
+                          default=[]),
+    ]
 
-if klein == 'j':
-    inhalt += string.ascii_lowercase
-    tester1=1;
-if gross == 'j':
-    inhalt += string.ascii_uppercase
-    tester2=1;
-if zahl == 'j':
-    inhalt += '0123456789'
-    tester3=1;
-if zeichen == 'j':
-    inhalt += string.punctuation
-    tester4=1;
+    answers = inquirer.prompt(abfrage)
 
-    alle_Tester=tester1+tester2+tester3+tester4
-    print("Sicherheitslevel "+str(alle_Tester))
+    gegebeneAntworten = str(answers)
 
+    inhalt = ''
+    if gegebeneAntworten.__contains__("Kleinbuchstaben"):
+        inhalt += string.ascii_lowercase
+    if gegebeneAntworten.__contains__("Grossbuchstaben"):
+        inhalt += string.ascii_uppercase
+    if gegebeneAntworten.__contains__("Zahlen[0-9]"):
+        inhalt += string.digits
+    if gegebeneAntworten.__contains__("Zeichen"):
+        inhalt += string.punctuation
 
-#GENERIERUNG DES PASSWORTS:
-pw= ''.join(random.choice(inhalt) for i in range(laenge))
-print("Ihr sicheres Passwort lautet:", pw)
-
-#wischenablage Kopieren
-pyperclip.copy(pw)
+    pw = ''.join(random.choice(inhalt) for i in range(laenge))
 
 
+_generate_password()
--- masterpass/passwordManager.py
+++ masterpass/passwordManager.py
@@ -1,236 +1,139 @@
-# Passwort initial wählbar
-# Vllt große textdatei mit beispiel wörtern als Vorgabe
-# # # Passwort muss änderbar sein
-# # # Passwort soll beim eingeben nicht im Klartext erscheinen (.getpass
-# # # (WICHTIG): Passwort wird in einer Textdatei abgelegt, beim starten des Programmes wird geschaut ob sich etwas in dieser Textdatei befindet
-# # # Passwort sollte verschlüsselt werden
-# # # sinnvolle Zeitspanne für erneute Abfrage vom Passwort
-<<<<<<< HEAD
-=======
-
->>>>>>> 04915b2 (Initial commit)
-import os
 from datetime import datetime
 from datetime import timedelta
 from random import randint
+import inquirer
+from getpass import getpass
 
-import maskpass
-from cryptography.fernet import Fernet
+# unsere erstellte Liste fuer die Option von Beispielsaetzen
+RANDOM_SENTENCE_WORDLIST = "/Users/yunusemre/PycharmProjects/pythonprojekt/masterpass/liste.txt"
+
+# die Rockyou Datei mit den schlechtesten passwoertern
+ROCKYOU = "/Users/yunusemre/PycharmProjects/pythonprojekt/masterpass/100k.txt"
 
 
-def ueberpruefeGleichheit(passwort):
-    zeitPunktAnfangMethode=datetime.now()
-    pruefer = maskpass.advpass("Geben sie ihr passwort erneut ein")
-    zeitPunktEndeMethode=datetime.now()
-    zwischenZeit=timedelta.total_seconds(zeitPunktEndeMethode-zeitPunktAnfangMethode)
-    if(zwischenZeit<300):
-<<<<<<< HEAD
-## if anweisung überprüft, ob länger als 5 minuten nix eingegeben wurde
-=======
-#if anweisung überprüft, ob man länger als 5 minuten nichts eingibt
->>>>>>> 04915b2 (Initial commit)
-        if (passwort == pruefer):
-            print("akzeptiert")
-            passwortVerstauen(passwort)
+# Ueberpruefen ob zweite eingabe beim bestimmen vom Mpw, gleich ist wie die erste
+def check_second_mpw_input(passwort):
+    zeitPunktAnfangMethode = datetime.now()
+    pruefer = getpass("[?]Geben Sie ihr Passwort erneut ein")
+    zeitPunktEndeMethode = datetime.now()
+    zwischenZeit = timedelta.total_seconds(zeitPunktEndeMethode - zeitPunktAnfangMethode)
+    if zwischenZeit <= 60:
+        # if anweisung überprueft, ob laenger als 5 minuten nix eingegeben wurde
+        if passwort == pruefer:
+            print("[!] Akzeptiert")
+            # passwortVerstauen(passwort)
 
-        elif (passwort!= pruefer):
-            print("Die Eingabe hat nicht übereingestimmt mit der zuvor, auf ein neues")
-            passwortBestimmen()
+        else:
+            print("[!] Die Eingabe hat nicht übereingestimmt mit der zuvor, auf ein neues")
+            set_password()
     else:
-        print(str(300-(zwischenZeit/60))+"min zu lange untätig")
-        print("sie wurden rausgeworfen, da sie zu lange untätig waren")
-        passwortBestimmen()
+        print(str(zwischenZeit - 60) + "[!] Sekunden zu lange untätig")
+        print("[!]Sie wurden rausgeworfen, da Sie zu lange untätig waren")
+        set_password()
 
 
-def passwortVerstauen(passwort):
-    key = Fernet.generate_key()
-    crypter = Fernet(key)
-    with open('key.txt', 'wb') as file:
-
-        file.write(key)
-
-    bPW = bytes(passwort, 'utf8')
-    with open('passwort.txt', 'wb') as file:
-        file.write(crypter.encrypt(bPW))
-
-def ueberpruefePasswortLaenge(passwort):
+# ueberprueft die Laenge des Passwortes
+def check_length_mpw_input(passwort):
     while passwort.__len__() < 7:
-        print("zu kurz!")
-        satzZufälligGebildet()
-        passwort = maskpass.advpass("")
+        print("[!] zu kurz")
+        generate_random_sentence()
+        passwort = getpass("")
     return passwort
 
-<<<<<<< HEAD
-### überpürft ob es bereits ein Passwort gibt(es wird geschaut nach "passwort.txt" und "key.txt)
-=======
->>>>>>> 04915b2 (Initial commit)
-def pruefExistPassW():
-    tester=False
-    try:
-        f=open('key.txt')
-        t1=open('passwort.txt')
 
-        tester=True
-    except Exception:
-        print("Noch kein Passwort erstellt")
-    if tester==True:
-        pwEinlesenVonTextData()
-    elif tester==False:
-        passwortBestimmen()
-<<<<<<< HEAD
-        
-        
-=======
->>>>>>> 04915b2 (Initial commit)
-
-def pwEinlesenVonTextData():
-    print("Es gibt bereits ein Passwort")
-    #pwEingabe = maskpass.advpass("Geben sie ihr Passwort ein um fortzufahren")
-    #bPwEingabe=bytes(pwEingabe,"utf-8")
-
-    with open('key.txt', 'rb') as file:
-        key = file.read()
-        crypter = Fernet(key)
-
-    with open('passwort.txt', 'rb') as file:
-        pw = file.read()
-        decryptetPW = crypter.decrypt(pw)
-    '''if (decryptetPW==bPwEingabe):
-        print("richtige Eingabe")
-        menuFuerPwOptionen()'''
-    pwEingabeBeiProgrammStart(decryptetPW)
-
-<<<<<<< HEAD
-#hier wird die Eingabe vom Nutzer mit dem gespeichertem Passwort verglichen
-=======
-
->>>>>>> 04915b2 (Initial commit)
-def pwEingabeBeiProgrammStart(decryptetPW):
-    x = pruefeAnzahlVersucheMasterPassW()
+# hier wird die Eingabe vom Nutzer mit dem gespeichertem Passwort verglichen
+# wahrscheinlich nicht wichtig, da wir nicht viel mit textdateien arbeiten wollen
+def mpw_input_program_start(decryptetPW):
+    x = check_wrong_inputs_mpw_program_start()
     while x > 0:
-        zeitPunktAnfangMethode=datetime.now()
-        pwEingabe = maskpass.advpass("Geben sie ihr Passwort ein um fortzufahren " + str(x) + " Versuch/e verbleibend")
-        zeitPunktEndeMethode=datetime.now()
+        zeitPunktAnfangMethode = datetime.now()
+        pwEingabe = getpass("[?]Geben sie ihr Passwort ein um fortzufahren " + str(x) + " Versuch/e verbleibend")
+        zeitPunktEndeMethode = datetime.now()
         bPwEingabe = bytes(pwEingabe, "utf-8")
-        zwischenZeit=timedelta.total_seconds(zeitPunktEndeMethode-zeitPunktAnfangMethode)
-        if(zwischenZeit<200):
-            if (bPwEingabe == decryptetPW):
-                print("richtige Eingabe")
+        zwischenZeit = timedelta.total_seconds(zeitPunktEndeMethode - zeitPunktAnfangMethode)
+        if zwischenZeit < 200:
+            if bPwEingabe == decryptetPW:
+                print("[!] richtige Eingabe")
                 with open('versucheBleibend.txt', 'w') as file:
-                    x=3
+                    x = 3
                     file.write(str(x))
-                menuFuerPwOptionen()
-                #x = 0
             x = x - 1
             with open('versucheBleibend.txt', 'w') as file:
                 file.write(str(x))
         else:
             print(zwischenZeit)
-            print("programm geschlossen, zu lange untätig")
+            print("[!] Programm geschlossen, zu lange untätig")
             return
 
-<<<<<<< HEAD
-#In Datei "versucheBleibend.txt" werden Restversuche gespeichert und abgelegt.
-=======
 
->>>>>>> 04915b2 (Initial commit)
-def pruefeAnzahlVersucheMasterPassW():
+# In Datei "versucheBleibend.txt" werden Restversuche gespeichert und abgelegt.
+# wahrscheinlich nicht wichtig, da wir nicht viel mit textdateien arbeiten wollen
+def check_wrong_inputs_mpw_program_start():
     try:
         with open('versucheBleibend.txt', 'r') as file:
             Z = file.read()
             x = int(Z)
-            if (x < 1):
-                print("sie wurden gesperrt")
+            if x < 1:
+                print("[!] Sie wurden gesperrt")
 
     except:
         x = 3
     return x
 
 
-def menuFuerPwOptionen():
-    optionen = maskpass.advpass("Falls sie ihr passwort anzeigen wollen, drücken sie die 1\n"
-                                "Falls sie ihr passwort ändern wollen, drücken sie die 2\n"
-                                "Falls sie das programm beenden wollen, drücken sie die 3\n"
-                                )
-
-<<<<<<< HEAD
-    # Option 1 noch nicht gemacht. 
-=======
->>>>>>> 04915b2 (Initial commit)
-    if(optionen=='2'):
-        fileKey = r"C:\Users\Arvand\PycharmProjects\Uni1\tester\key.txt"
-        filePassW=r"C:\Users\Arvand\PycharmProjects\Uni1\tester\passwort.txt"
-        ##os.remove(fileKey)
-        ##os.remove(filePassW)
-        passwortBestimmen()
-    elif(optionen=='3'):
-        print("Programm beendet")
-        exit()
-
-<<<<<<< HEAD
-#hier wird die Datei "100k.txt" abgeglichen mit der Eingabe, falls fündig wird ein True zurückgegeben 
-=======
-
->>>>>>> 04915b2 (Initial commit)
-def sicherheitsTest(passwort):
-    fopen= open('100k.txt', 'r')
-    fread= fopen.readlines()
-
-    pwTest=True
-    while(pwTest==True):
-        pw= str(passwort)
-        pw.lower()
+# hier wird die Datei "100k.txt->ROCKYOU" abgeglichen mit der Eingabe, falls fuendig wird ein True zurückgegeben
+def is_in_rockyou(passwort):
+    with open(ROCKYOU, "r") as fopen:
+        fread = fopen.readlines()
         for line in fread:
-
-            if pw in line:
+            if passwort == line.strip():
                 return True
-                break
-        pwTest=False
-    return False
+        return False
 
-<<<<<<< HEAD
 
-#hier werden mithilfe der  Datei "liste.txt" drei Vorschläge für den Nutzer gemacht für ein sicheres Passwort
-=======
->>>>>>> 04915b2 (Initial commit)
-def satzZufälligGebildet():
-    print("Geben sie hier ihr Passwort ein(länger als 7 Zeichen).\n"
-          "WICHTIG! Schreiben sie für ein sicheres Passwort zunächst einmal einen unüblichen Satz auf ein Blatt Papier, wie Z.B:")
-    zufallszahl=randint(1,93)
-    fopen= open('liste.txt', 'r')
-    fread=fopen.readlines()
-    data=[]
+# hier werden mithilfe der  Datei "liste.txt" drei Vorschlaege für den Nutzer gemacht für ein sicheres Passwort
+def generate_random_sentence():
+    print("WICHTIG! Schreiben sie für ein sicheres Passwort zunächst einmal einen unüblichen Satz auf ein Blatt "
+          "Papier\nwie z.B einen dieser drei Sätze:")
+    with open(RANDOM_SENTENCE_WORDLIST, 'r') as fopen:
+        fread = fopen.readlines()
+    data = []
     for line in fread:
-        line=line.strip()
+        line = line.strip()
         data.append(line)
-    #s=""
-    zufallszahlSatzLaenge=randint(7,12)
-    for i in range(0,3):
-        s=""
+    zufallszahlSatzLaenge = randint(7, 12)
+    for i in range(0, 3):
+        s = ""
         for z in range(zufallszahlSatzLaenge):
-            s=(s + data[randint(1,1000)]+" ")
+            s = (s + data[randint(1, 1000)] + " ")
         print(s)
-    print("\nFalls sie dies getan haben, sollten sie nun einfach aus jedem Wort ein bis zwei buchstaben\n"
-          "für ihr passwort aussuchen. Markieren sie sich auf ihrer Notiz die jeweiligen Buchstaben\n"
-          "und achten sie dabei, mindestens 8 Zeichen zu haben.")
-
-def passwortBestimmen():
-    #dummePWdaTa()
-    satzZufälligGebildet()
-    passwort = maskpass.advpass("")
-
-    '''if sicherheitsTest(passwort):
-        print("Nicht sicher genug,anderes Passwort wählen!")
-        passwortBestimmen()'''
-    ueberpruefePasswortLaenge(passwort)
-    #sicherheitsTest(passwort)
-    ueberpruefeGleichheit(passwort)
-    # passwortaendern(passwort)
-
-pruefExistPassW()
+    print("\n[!] Falls Sie dies getan haben, sollten Sie nun einfach aus jedem Wort ein bis zwei Buchstaben\n"
+          "für ihr Passwort aussuchen. Markieren Sie sich auf ihrer Notiz, die jeweiligen Buchstaben.\n")
 
 
+# wird als erstes aufgerufen
+def set_password():
+    questions = [
+        inquirer.List('Passwort',
+                      message="[?] Möchten Sie Hilfe beim erstellen des Passworts",
+                      choices=['JA', 'NEIN'],
+                      ),
+    ]
+    answers = inquirer.prompt(questions)
+
+    gegebeneAntwort = str(answers)
+    if gegebeneAntwort.__contains__("JA"):
+        generate_random_sentence()
+    passwort = getpass("[?]Passwort eingeben(länger als 7 Zeichen)")
+
+    if is_in_rockyou(passwort):
+        print("[!]Befindet sich in der ROCKYOU Datei, somit nicht sicher genug")
+        set_password()
+    # möglicherweise nicht funktionsfähig, bei der methode bekomme ich Error, der nicht weg gehen will
+    # bei Yunus funktioniert es allerdings
+
+    check_length_mpw_input(passwort)
+    check_second_mpw_input(passwort)
 
 
-
-
+set_password()
